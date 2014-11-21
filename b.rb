d297844d76ebf4ec5c7f658326df618df1598896
SeeWhyAnEyeSee <cyniclimbu@gmail.com>
2014-11-22 06:42:22 +0800

    Fix for script-detect issue
    This version only patches a code that wasn't able to check whether self
    running script was .exe or .rb
    
    Nothing else has been updated.

--- b.rb
+++ b.rb
@@ -4,12 +4,14 @@ require 'win32ole'
 
 include HighLine::SystemExtensions
 
+exit if defined?(Ocra)
+
 RootShell = "rootShell"
 ERRORMSG = " An unknown error occured.\n Please send an email to cyniclimbu@gmail.com\n Describing what caused it.".bold.cyan
 OUTTAATTEMPTS = "OUT OF ATTEMPTS\n   Restart the shell to try again.".bold.white
 
 
-#--------------------METHOD--------------------# 001
+#--------------------METHOD--------------------# 001 COMPLETED!
 def run_rootShell_as_admin
 puts "Making a shell for you :]".bold.green
 
@@ -60,7 +62,7 @@ end
 
 #--------------------#----#--------------------#
 
-#--------------------METHOD--------------------# 002
+#--------------------METHOD--------------------# 002 NOT COMPLETED
 def install #INSTALL B01
 #------------------------------------------------
 puts " INSTALL".bold.blue
@@ -100,19 +102,29 @@ end #END B01
 
 
 def admin? #ADMIN? D01
-
+#--------------------------------------------------------
+#DEFINE VARIABLES BEFOREHAND!
 reg_key  = 'reg query "HKU\S-1-5-19"'
 is_admin = system(reg_key)
 
+self_name = File.basename $0,".rb" 
+self_exe  = self_name + ".exe"
+self_rb   = self_name  + ".rb"
+#--------------------------------------------------------
 if is_admin == true # IF D02
 system("cls")
 install # M E T H O D
 #--------------------------------------------------------
 elsif is_admin == false # ELSIF D02
 system("cls")
+
+if File.exists?(self_exe) and File.exists?(self_rb) # IF D03
+puts "Error, A file with a same name was found on this directory.".bold.red
+puts "Please move the copy somewhere else and restart this shell.".bold.red
+sleep 4; exit
 #--------------------------------------------------------
-if $0 =~ /.rb\z/i # IF D03
-				  # IF FILE EXTENSION IS IN .rb OR .RB 
+elsif File.exists?(self_rb) # ELSIF D03
+							# IF FILE EXTENSION IS IN .rb OR .RB 
 puts " Installer must run as administrator to begin.\n"
 puts "\n Message from my creator :D".bold.cyan
 puts " I can just create a batch file for you to run it as administrator.".bold.cyan
@@ -126,7 +138,7 @@ choice = get_character.chr
 if choice == 'y' or choice == 'Y' # IF D0312
 #------------------------------------------------
 
-run_rootShell_as_admin # M E T H O D
+puts "run_rootShell_as_admin" # M E T H O D
 
 #------------------------------------------------
 elsif choice == 'n' or choice == 'N' # ELSIF D0312
@@ -149,19 +161,16 @@ end  # END D0312
 loop += 1
 end # END D031
 #------------------------------------------------
-elsif $0 =~ /.exe\z/i # ELSIF D03
-					  # IF FILE EXTENSION IS IN .exe OR .EXE
+elsif File.exists?(self_exe) # ELSIF D03
+							 # IF FILE EXTENSION IS IN .exe OR .EXE
 puts " Shell isn't running as administrator.".bold.red
 puts " Execute this shell as administrator.".bold.red
 sleep 5; exit
 #------------------------------------------------
-else # ELSE D03
-puts " ERROR, REGEX FAILED OR RETURNED NIL ON .rb OR .exe".bold.red
+else # ELSE D03 #brtvnckdrtbnvjcs
+puts " ERROR, NO FILE WITH '#{self_name}' NAME WAS FOUND.".bold.red
 puts " ERROR CODE - A0".bold.red
-puts " Try restarting.".bold.white
-puts "         OR"
-puts " Contact - cyniclimbu@gmail.com".bold.cyan
-
+puts " Try restarting.\n".bold.white
 puts " Press 'M' to see more detail information.".bold.white
 puts " Press 'E' to exit shell.".bold.white
 #------------------------------------------------
@@ -169,12 +178,17 @@ loop = 0; while loop < 7 #WHILE LOOP D04
 print " > "
 choice = get_character.chr
 if choice == 'm' or choice == 'M' # IF D04
-puts "\n CODE - A0".bold.red
-puts " A REGEX IS SUPPOSED TO PICKUP THE FILE EXTENSION.".bold.green
-puts " OF THIS FILE AND FIGURE OUT IF IT's .rb or .exe (case insensitive)".bold.green
-puts " JUST SEND ME AN E-MAIL, ALLRIGHT? :)".bold.white
+puts "\n CODE - A0".bold.red #--# ERROR CODE A0 #--#
+puts " A CODE WAS SUPPOSED TO CHECK IF SELF-RUNNING FILE WAS .exe or .rb".bold.green
+puts " Possible reason for this error: ".bold.green
+puts " 1.The original name of this file was changed!"
+puts "    Revert the name if you changed it before."
+puts " 2.The extension was changed from .rb to .RB or from .exe to .EXE"
+puts "   Revert the extension name if you changed it before."
+sleep 4; exit
 #------------------------------------------------
 elsif choice == 'e' or choice == 'E' # ELSIF D04
+
 bye = ["B","y","e"," ","b","y","e","!"]
 bye.each do |exitmsg|
 print exitmsg.bold.magenta 
